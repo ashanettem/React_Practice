a0a0e1d2bf9d4694ab4a1e881e4fc17397f7084a
Amanda Williams <48529505+ashanettem@users.noreply.github.com>
2022-07-17 09:29:16 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Cars I Like</h1>
+    <ul>
+      <li>GMC Yukon</li>
+      <li>BMW X5</li>
+      <li>Mercedes GLE 350</li>
+    </ul>
+  </div>,
+  document.getElementById("root")  
+);
+
